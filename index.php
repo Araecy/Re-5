92ada2053c68aeeea77bbcbb249311b913e211c9
Araecy <74868163+Araecy@users.noreply.github.com>
2022-05-17 00:02:21 +0200

    Basic structure.

--- index.php
+++ index.php
@@ -5,14 +5,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.min.css">
     <title>ProjectWeek2</title>
 </head>
 
 <body>
-    <a href="#">HTML</a>
-    <a href="#">CSS</a>
-    <a href="#">JavaScript</a>
-    <a href="#">Python</a>
+
+    <div id="header">
+        
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#nav-1">Computers</a></li>
+            <li><a href="#nav-2">Classrooms Setup</a></li>
+            <li><a href="#nav-3">Why?</a> </li>
+            <li><a href="#nav-4">About us</a></li>
+        </ul>
+    </nav>
+
+
+    <section id="nav-1">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro illo reprehenderit debitis, quasi nulla eius ducimus magnam unde veritatis laboriosam libero nobis autem voluptate optio modi rerum dolorem quod earum incidunt nisi. Eaque, quas cumque, sunt magni eos obcaecati ut vel voluptas exercitationem atque illo corrupti! Iure deleniti qui iusto.</p>
+    </section>
+    <section id="nav-1">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro illo reprehenderit debitis, quasi nulla eius ducimus magnam unde veritatis laboriosam libero nobis autem voluptate optio modi rerum dolorem quod earum incidunt nisi. Eaque, quas cumque, sunt magni eos obcaecati ut vel voluptas exercitationem atque illo corrupti! Iure deleniti qui iusto.</p>
+    </section>
 </body>
 
 </html>
